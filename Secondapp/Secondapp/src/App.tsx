b30f1942552b85ad7d6fae714f3adcb037daa990
Saketh2714 <sakethchowdhary167@gmail.com>
2024-12-19 15:27:53 +0530

    first commit

---
+++ Secondapp/Secondapp/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './component/NavBar'
+import Home from './component/Home'
+
+export default function App() {
+  return (
+    <div>
+
+
+
+      
+    </div>
+  )
+}
